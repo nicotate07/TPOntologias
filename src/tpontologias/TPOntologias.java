d4d7422516a354f22d74d62dab40b2a2161bc40d
Nicolas Gonzalez Paz <nico_tate07@hotmail.com>
2018-06-27 22:11:06 -0300

    Primer commit con conexion a DB

---
+++ src/tpontologias/TPOntologias.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpontologias;
+
+/**
+ *
+ * @author Nico
+ */
+public class TPOntologias {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		Pantalla p = new Pantalla();
+		p.setVisible(true);
+	}
+	
+}
